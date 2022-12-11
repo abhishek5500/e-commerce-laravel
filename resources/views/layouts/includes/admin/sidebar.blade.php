59b3b48df3cf2dc3c6cd3cc5f006574cd17eebdc
Abhishek Chaubey <abhishekchaubey430@gmail.com>
2022-12-11 14:45:47 +0530

    Admin sidebar Active

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -1,19 +1,19 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
           <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/dashboard')}}">
+            <a class="nav-link {{Request::is('admin/dashboard') ? 'active':''}}" href="{{url('admin/dashboard')}}">
               <i class="mdi mdi-home menu-icon"></i>
               <span class="menu-title ">Dashboard</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/orders')}}">
+            <a class="nav-link {{Request::is('admin/dashboard') ? 'active':''}}" href="{{url('admin/orders')}}">
               <i class="mdi mdi-sale menu-icon"></i>
               <span class="menu-title">Orders</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <a class="nav-link {{Request::is('admin/products') ||('admin/products/create') ? 'active':''}}" data-bs-toggle="collapse" href="#ui-basic"  >
               <i class="mdi mdi-plus-circle menu-icon"></i>
               <span class="menu-title">Products</span>
             </a>
@@ -25,43 +25,43 @@
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="pages/charts/chartjs.html">
+            <a class="nav-link " href="pages/charts/chartjs.html">
               <i class="mdi mdi-chart-pie menu-icon"></i>
               <span class="menu-title">Charts</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="pages/tables/basic-table.html">
+            <a class="nav-link " href="pages/tables/basic-table.html">
               <i class="mdi mdi-grid-large menu-icon"></i>
               <span class="menu-title">Tables</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/category')}}">
+            <a class="nav-link {{Request::is('admin/category') ? 'active':''}}" href="{{url('admin/category')}}">
               <i class="mdi mdi-emoticon menu-icon"></i>
               <span class="menu-title">Category</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/brands')}}">
+            <a class="nav-link {{Request::is('admin/brands') ? 'active':''}}" href="{{url('admin/brands')}}">
               <i class="mdi mdi-emoticon menu-icon"></i>
               <span class="menu-title">Brands</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/colors')}}">
+            <a class="nav-link {{Request::is('admin/colors') ? 'active':''}}" href="{{url('admin/colors')}}">
               <i class="mdi mdi-emoticon menu-icon"></i>
               <span class="menu-title">Colors</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{url('admin/sliders')}}">
+            <a class="nav-link {{Request::is('admin/sliders') ? 'active':''}}" href="{{url('admin/sliders')}}">
               <i class="mdi mdi-emoticon menu-icon"></i>
               <span class="menu-title">Home Sliders</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <a class="nav-link {{Request::is('admin/users')|| ('admin/users/create')? 'active':''}}" data-bs-toggle="collapse" href="#auth" >
               <i class="mdi mdi-account menu-icon"></i>
               <span class="menu-title">User</span>
               <i class="menu-arrow"></i>
@@ -70,9 +70,7 @@
               <ul class="nav flex-column sub-menu">
                 <li class="nav-item"> <a class="nav-link" href="{{url('admin/users')}}"> Users</a></li>
                 <li class="nav-item"> <a class="nav-link" href="{{url('admin/users/create')}}"> Add Users </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+               
               </ul>
             </div>
           </li>
